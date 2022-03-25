39ebd1518bc3c8d3186527d4dea22f9f57b70e7d
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-03-25 11:12:11 -0300

    relacionamentos

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const connection = require("./database/database")
 const categoriesController = require("./categories/CategoriesController");
 const articlesController = require("./articles/ArticlesController");
 
+const Article = require("./articles/Article");
+const Category = require("./categories/Category");
+
 // view engine para exibir html
 app.set('view engine', 'ejs')
 
