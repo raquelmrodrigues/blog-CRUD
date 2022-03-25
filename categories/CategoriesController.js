e13fe52d8d2ee04d29a16737d26e92f87d4a2dc6
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-03-25 15:21:54 -0300

    deleção

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,56 @@
 const express = require("express");
+const { default: slugify } = require("slugify");
 const router = express.Router();
+const Category = require("./Category");
 
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new")
+});
+
+router.post("/categories/save", (req, res) => {
+    var title = req.body.title;
+    if(title != undefined){
+
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect("/");
+        })
+
+    }else{
+        res.redirect("/admin/categories/new")
+    }
+});
+
+router.get("/admin/categories", (req, res) => {
+
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", {categories:categories});
+    });
+});
+
+// deletar uma categoria
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id;
+    if(id != undefined) {
+        if(!isNaN(id)) {
+
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories")
+            });
+
+        }else{
+            res.redirect("/admin/categories");
+        }
+
+    }else{
+        res.redirect("/admin/categories");
+    }
 });
 
 module.exports = router;
