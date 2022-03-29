0010f43b9c379661649cc409cf712fe346119a15
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-03-29 12:30:03 -0300

    pagina de de edição de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -56,4 +56,21 @@ router.post("/articles/delete", (req, res) => {
     }
 });
 
+// rota de edição
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id;
+    Article.findByPk(id).then(article => {
+        if(article != null) {
+            Categoru.findAll().then(categories => {
+                res.render("admin/articles/edit" {categories: categories})
+            });
+            
+        } else {
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    });
+});
+
 module.exports = router;
