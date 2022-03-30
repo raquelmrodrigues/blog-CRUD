f008c4af97b67f8de93118e3a6c6ed90bb427502
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-03-29 22:12:21 -0300

    persistindo edições

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -62,7 +62,7 @@ router.get("/admin/articles/edit/:id", (req, res) => {
     Article.findByPk(id).then(article => {
         if(article != null) {
             Categoru.findAll().then(categories => {
-                res.render("admin/articles/edit" {categories: categories})
+                res.render("admin/articles/edit", {categories: categories})
             });
             
         } else {
@@ -72,5 +72,23 @@ router.get("/admin/articles/edit/:id", (req, res) => {
         res.redirect("/");
     });
 });
+
+// atualiza edição no artigo
+router.post("/articles/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category
+
+    Article.update({title: title, body: body, categoryId: category, slug:slugify(title)}, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    }).catch(err => {
+        res.redirect("/");
+    })
+})
 
 module.exports = router;
